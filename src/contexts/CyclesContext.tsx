76f66c365268e8f89e0a7e74aebae9d8695b59e9
Carlos <devcarlos0112@gmail.com>
2025-09-29 15:58:21 -0300

    Correção de bugs

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, useReducer, useState, type ReactNode } from "react";
+import { createContext, useEffect, useReducer, useState, type ReactNode } from "react";
 import { cyclesReducer, type Cycle } from "../reducers/cycles/reducer";
 import { criarNovoCicloAction, interromperCicloAction, marcarCicloAtivoComoConcluidoAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 //interfaces
 
@@ -41,14 +42,42 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     const [cyclesState, dispatch] = useReducer(cyclesReducer, {
         cycles: [],
         activeCycleId: null
+    },
+    (initialState) => {
+        const storageCyclesState = localStorage.getItem('@ignite-timer:cycles-state-1.0.0');
+
+        if(storageCyclesState) {
+
+            return JSON.parse(storageCyclesState);
+
+        }
+
+        return initialState;
     });
-
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
 
     const {cycles, activeCycleId} = cyclesState
 
     const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+        
+        if(activeCycle) {
+            return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+        }
+
+        return 0
+
+    });
+
+    
+
+    //useEffect
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState)
+
+        localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+
+    }, [cyclesState])
 
     //funções
 
